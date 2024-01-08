cbb95bba9502a81129afc2647959ffec681e685e
Puneeth2004 <155820543+Puneeth2004@users.noreply.github.com>
2024-01-08 18:55:49 +0530

    Delete SumOfNumbers1.java

--- SumOfNumbers1.java
+++
@@ -1,9 +0,0 @@
-public class SumOfNumbers1  
-{  
-public static void main(String args[])   
-{  
-int n1 = 18, n2 =17 , sum;  
-sum = n1 + n2;  
-System.out.println("The sum of numbers is: "+sum);  
-}  
-}  
